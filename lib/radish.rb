05354727e301a96f4f1f8d010e96415fbacfd800
Don Kelly <karfai@gmail.com>
2017-02-12 10:31:41 -0500

    Import randomness code from other projects

--- lib/radish.rb
+++ lib/radish.rb
@@ -1,5 +1,7 @@
 require_relative './radish/collection'
+require_relative './radish/randomness'
 
 R = Class.new do |cl|
   cl.include(Radish::Collection)
+  cl.include(Radish::Randomness)
 end.new
--- spec/radish_spec.rb
+++ spec/radish_spec.rb
@@ -3,5 +3,6 @@ describe Radish do
     require_relative '../lib/radish'
     expect(R).to_not be_nil
     expect{ R.method(:each) }.to_not raise_error
+    expect{ R.method(:rand_one) }.to_not raise_error
   end
 end
